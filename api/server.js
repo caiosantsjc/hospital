e4b7b1a94933147ee6bc0c88cef989c022bb9a2b
caiosantsjc <caiosantanaprofissional@gmail.com>
2024-10-22 11:31:20 -0300

    criando conexao com db usando mongoose no arquivo server.js

--- api/server.js
+++ api/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
+const mongoose = require(`mongoose`);
 const port = 2105;
 
 
-app.get("/", (request, response) => {
-    response.send('Hello World');
-})
+const nomeDB = "db_hospital";
+
+app.use(express.json());
+
+
+mongoose.connect(`mongodb://localhost:27017/${nomeDB}`)
+    .then(() => console.log('Conectado ao MongoDB'))
+    .catch((err) => console.log('Erro ao conectar ao MongoDB:', err));
 
 app.listen(port, () => {
     console.log(`App rodando em http://localhost:${port}`);
