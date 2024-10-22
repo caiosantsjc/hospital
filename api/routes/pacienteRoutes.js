06bc84811a141673b284ea9c0140a6d743e7dd81
caiosantsjc <caiosantanaprofissional@gmail.com>
2024-10-22 15:03:02 -0300

    criar rota para deletar usuario

--- api/routes/pacienteRoutes.js
+++ api/routes/pacienteRoutes.js
@@ -6,7 +6,7 @@ const Paciente = require('../models/Paciente');
 router.get('/', async (request, response) => {
     try {
         const pacientes = await Paciente.find();
-        response.status(200).json(pacientes); // Corrigido para 'response'
+        response.status(200).json(pacientes);
     } catch (err) {
         response.status(500).json({ error: err.message });
     }
@@ -15,7 +15,7 @@ router.get('/', async (request, response) => {
 // Retorna um paciente pelo CPF
 router.get('/paciente/:CPF', async (request, response) => {
     try {
-        const paciente = await Paciente.findById(request.params.CPF);
+        const paciente = await Paciente.findOne({ CPF: request.params.CPF }); // Corrigido para buscar pelo CPF
         if (!paciente) {
             return response.status(404).json({ message: 'Paciente não encontrado' });
         }
@@ -26,15 +26,29 @@ router.get('/paciente/:CPF', async (request, response) => {
 });
 
 // Cadastrar um paciente
-router.post('/adicionarPaciente', async (request, response) => { // Adicionada a barra '/'
+router.post('/cadastrarPaciente', async (request, response) => {
     try {
         const newPaciente = new Paciente(request.body);
         await newPaciente.save();
-        response.status(201).json(newPaciente); // Corrigido para 'response'
+        response.status(201).json(newPaciente);
     } catch (error) {
         response.status(400).json({ error: error.message });
     }
 });
+
+// Deletar um Paciente
+router.delete('/removerPaciente/:CPF', async (request, response) => {
+    try {
+        const deletedPaciente = await Paciente.findOneAndDelete({ CPF: request.params.CPF }); // Corrigido para buscar pelo CPF
+        if (!deletedPaciente) {
+            return response.status(404).json({ message: 'Paciente não encontrado' });
+        }
+        response.status(200).json({ message: 'Paciente removido com sucesso' });
+    } catch (error) {
+        response.status(500).json({ error: error.message }); // Alterado para 500 em caso de erro do servidor
+    }
+});
+
 
 
 module.exports = router;
