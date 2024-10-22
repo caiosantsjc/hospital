253180b97ba8586fdf5d78587600224e8ced4fd6
caiosantsjc <caiosantanaprofissional@gmail.com>
2024-10-22 16:06:22 -0300

    Alterar o App.jsx e configurar os estilos em App.css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,18 @@
+// src/App.jsx
+
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './views/Home';
 import NotFound from './views/NotFound';
+import './App.css'; // Importando o CSS
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route component={NotFound} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </Router>
   );
 };
